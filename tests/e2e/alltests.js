b282863d0496d5e87a3105f51bb7c9db8eba4821
David Sharpe <david.a.sharpe@cgi.com>
2020-06-08 21:29:37 -0700

    Add a search test.

--- tests/e2e/alltests.js
+++ tests/e2e/alltests.js
@@ -1,15 +1,32 @@
 import { Selector } from 'testcafe';
+import { Role } from 'testcafe';
 
-fixture `All tests`
-    .page `http://localhost:8080`;
-    // .page `https://common-logon-dev.hlth.gov.bc.ca/webapp/`;
-
-test('Test login', async t => {
+const regularAccUser = Role('http://localhost:8080', async t => {
     await t
         .click('#zocial-moh_idp')
         .typeText('#username', 'testcafe')
         .typeText('#password', process.env.TESTCAFE_PASSWORD)
-        .click("#kc-login")
+        .click("#kc-login");
+});
+
+
+fixture
+    .disablePageCaching `All tests`
+    .page `http://localhost:8080`;
+
+test('Smoke test', async t => {
+    await t
+        .useRole(regularAccUser)
         .expect(Selector('#search-button').exists)
         .ok();
+});
+
+test('Test search', async t => {
+    await t
+        .useRole(regularAccUser)
+        .typeText('#user-search', 'testcafe')
+        .click('#search-button')
+        .expect(Selector('html').textContent)
+        // This is the ID of the testcafe user.
+        .contains('3195a1bf-4bea-47c4-955d-cf52d4e2fc15');
 });
