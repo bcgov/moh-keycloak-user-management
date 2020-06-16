b2b0e10f3611e848dc1a7f0bc8ef40863c1839ac
Trevor Schiavone <tschia@outlook.com>
2020-06-16 12:17:31 -0700

    update user info
    Added comments, tooltips, error messaging on role update failure, and refactored v-on and v-bind for consistency.

--- src/api/UsersRepository.js
+++ src/api/UsersRepository.js
@@ -21,14 +21,17 @@ export default {
         return kcRequest().then(axiosInstance => axiosInstance.put(`${resource}/${userId}`, content));
     },
 
+    /*Get available client-level roles that can be mapped to the user*/
     getUserAvailableClientRoles(userId, clientId) {
         return kcRequest().then(axiosInstance => axiosInstance.get(`${resource}/${userId}/${clientRoleMappings}/${clientId}/available`));
     },
 
+    /* Get active client-level role mappings. Active roles include only those that have been directly added to the user and can be removed */
     getUserActiveClientRoles(userId, clientId) {
         return kcRequest().then(axiosInstance => axiosInstance.get(`${resource}/${userId}/${clientRoleMappings}/${clientId}`));
     },
 
+    /* Get effective client-level role mappings. Effective roles are all client-level roles assigned to a user including those provided by group membership or by composite roles */
     getUserEffectiveClientRoles(userId, clientId) {
         return kcRequest().then(axiosInstance => axiosInstance.get(`${resource}/${userId}/${clientRoleMappings}/${clientId}/composite`));
     },
