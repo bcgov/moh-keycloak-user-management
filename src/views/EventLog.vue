ce22685408b35b45aa9de8b9a9361b0aebe2e762
David Sharpe <david.a.sharpe@cgi.com>
2020-06-19 21:49:15 -0700

    AB#750 First draft event log. Issues Keycloak request for login and admin events. No styling.

--- src/views/EventLog.vue
+++ src/views/EventLog.vue
@@ -1,5 +1,68 @@
 <template>
-  <div >
-      EVENT LOG
-  </div>
+    <div>
+        <button @click="getEvents">Events</button> |
+        <button @click="getAdminEvents">Admin Events</button>
+        <ul>
+            <li v-for="(event, index) in events" :key="index">
+                {{event.niceDate}} | {{ event.type }} | {{ event.userId }} | {{event.details.username}}
+            </li>
+        </ul>
+        <ul>
+            <li v-for="(event, index) in adminEvents" :key="index">
+                {{event.niceDate}} | {{event.operationType}} | {{ event.resourceType }}
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script>
+    import {RepositoryFactory} from "./../api/RepositoryFactory";
+
+    const EventsRepository = RepositoryFactory.get("events");
+    const AdminEventsRepository = RepositoryFactory.get("adminEvents");
+
+    const options = {dateStyle: 'short', timeStyle: 'short'};
+    const formatDate = new Intl.DateTimeFormat(undefined, options).format;
+
+    export default {
+        name: "EventLog",
+        data() {
+            return {
+                events: [],
+                adminEvents: []
+            }
+        },
+
+        methods: {
+            getEvents: function () {
+                return EventsRepository.getEvents()
+                    .then(response => {
+                        this.events = response.data;
+                        for (let e of this.events) {
+                            e.readableDate = formatDate(e.time);
+                            if (!e.details) {
+                                e.details = 'no details';
+                            }
+                        }
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        throw e;
+                    });
+            },
+            getAdminEvents: function () {
+                return AdminEventsRepository.getEvents()
+                    .then(response => {
+                        this.adminEvents = response.data;
+                        for (let e of this.adminEvents) {
+                            e.readableDate = formatDate(e.time);
+                        }
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        throw e;
+                    });
+            }
+        }
+    };
+</script>
