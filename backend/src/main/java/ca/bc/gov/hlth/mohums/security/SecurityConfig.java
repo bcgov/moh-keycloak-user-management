4a6b76235c8efc194531d05891b16826fa8449d2
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-10-20 15:20:33 -0700

    add openapi and swagger docs

--- backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package ca.bc.gov.hlth.mohums.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -15,14 +17,24 @@ public class SecurityConfig {
     private String viewGroupsRole;
 
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain docsSecurityWebFilterChain(final ServerHttpSecurity http) {
+        // Disable security on the /docs path for Open API and Swagger Docs
+        http.securityMatcher(ServerWebExchangeMatchers.pathMatchers("/docs/*"));
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
 
         final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakClientRoleConverter());
 
         http
+            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/*"))
             .authorizeExchange()
-                .pathMatchers("/groups").hasRole(viewGroupsRole)
+                .pathMatchers("/groups/*").hasRole(viewGroupsRole)
             .anyExchange().authenticated().and()
             .oauth2ResourceServer().jwt()
             .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter));
