939d60a81e7c240468e38728a82b8305d0165375
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-10-21 14:42:24 -0700

    remove unused class used for initial testing

--- backend/src/main/java/ca/bc/gov/hlth/mohums/MoHUmsApplication.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/MoHUmsApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoHUmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoHUmsApplication.class, args);
 	}
-
-
 }
