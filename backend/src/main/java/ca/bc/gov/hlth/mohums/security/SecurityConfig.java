68afd38f6113da23ae8353e1451b3774aaeb6555
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-11-17 17:02:19 -0800

    fixed a defect with our role security implementation (#30)
    * fixed a defect with our role security implementation
    
    * Update SecurityConfig.java
    
    Removed unused import org.springframework.core.annotation.Order
    
    Co-authored-by: ahoplock <68440291+ahoplock@users.noreply.github.com>

--- backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package ca.bc.gov.hlth.mohums.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -19,30 +18,23 @@ public class SecurityConfig {
     @Value("${user-management-client.roles.view-groups}")
     private String viewGroupsRole;
 
-    @Value("${user-management-client.roles.view-users")
+    @Value("${user-management-client.roles.view-users}")
     private String viewUsersRole;
 
     @Bean
-    @Order(1)
-    public SecurityWebFilterChain docsSecurityWebFilterChain(final ServerHttpSecurity http) {
-        // Disable security on the /docs path for Open API and Swagger Docs
-        http.securityMatcher(ServerWebExchangeMatchers.pathMatchers("/docs/*"));
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
 
         final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakClientRoleConverter());
 
         http
-            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/*"))
+            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/**"))
             .authorizeExchange()
-                .pathMatchers("/clients/*").hasRole(viewClientsRole)
-                .pathMatchers("/groups/*").hasRole(viewGroupsRole)
-                .pathMatchers("/users/*").hasRole(viewUsersRole)
+                .pathMatchers("/docs/**").permitAll()
+                .pathMatchers("/clients/**").hasRole(viewClientsRole)
+                .pathMatchers("/groups/**").hasRole(viewGroupsRole)
+                .pathMatchers("/users/**").hasRole(viewUsersRole)
+                .pathMatchers("/*").denyAll()
             .anyExchange().authenticated().and()
             .oauth2ResourceServer().jwt()
             .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter));
