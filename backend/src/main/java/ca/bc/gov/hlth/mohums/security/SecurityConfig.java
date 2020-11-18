b67d17ebb70bf73fa62ed107780d046880da555c
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-11-17 16:49:11 -0800

    fixed a defect with our role security implementation

--- backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/security/SecurityConfig.java
@@ -19,30 +19,23 @@ public class SecurityConfig {
     @Value("${user-management-client.roles.view-groups}")
     private String viewGroupsRole;
 
-    @Value("${user-management-client.roles.view-users")
+    @Value("${user-management-client.roles.view-users}")
     private String viewUsersRole;
 
     @Bean
-    @Order(1)
-    public SecurityWebFilterChain docsSecurityWebFilterChain(final ServerHttpSecurity http) {
-        // Disable security on the /docs path for Open API and Swagger Docs
-        http.securityMatcher(ServerWebExchangeMatchers.pathMatchers("/docs/*"));
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
 
         final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakClientRoleConverter());
 
         http
-            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/*"))
+            .securityMatcher(ServerWebExchangeMatchers.pathMatchers("/**"))
             .authorizeExchange()
-                .pathMatchers("/clients/*").hasRole(viewClientsRole)
-                .pathMatchers("/groups/*").hasRole(viewGroupsRole)
-                .pathMatchers("/users/*").hasRole(viewUsersRole)
+                .pathMatchers("/docs/**").permitAll()
+                .pathMatchers("/clients/**").hasRole(viewClientsRole)
+                .pathMatchers("/groups/**").hasRole(viewGroupsRole)
+                .pathMatchers("/users/**").hasRole(viewUsersRole)
+                .pathMatchers("/*").denyAll()
             .anyExchange().authenticated().and()
             .oauth2ResourceServer().jwt()
             .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter));
