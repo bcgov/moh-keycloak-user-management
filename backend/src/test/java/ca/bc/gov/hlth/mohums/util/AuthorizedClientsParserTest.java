4b6ca24106e3dc61a3cc0d390fa4295a569033ad
ahoplock <68440291+ahoplock@users.noreply.github.com>
2020-11-19 16:21:23 -0600

    Update AuthorizedClientsParserTest.java
    Moving from JUnit 4 -> 5.

--- backend/src/test/java/ca/bc/gov/hlth/mohums/util/AuthorizedClientsParserTest.java
+++ backend/src/test/java/ca/bc/gov/hlth/mohums/util/AuthorizedClientsParserTest.java
@@ -1,8 +1,8 @@
 package ca.bc.gov.hlth.mohums.util;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
