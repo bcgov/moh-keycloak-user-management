ec15962654f57dd2fcfdb3581b2204bfc3d22a97
David Sharpe <david.a.sharpe@cgi.com>
2020-11-19 22:00:20 -0800

    Convert application from Spring WebFlux to Spring MVC.

--- backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/WebClientConfig.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/WebClientConfig.java
@@ -5,13 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -25,31 +25,33 @@ public class WebClientConfig {
     private String keycloakAdminBaseUrl;
 
     @Bean
-    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
-            final ReactiveClientRegistrationRepository clientRegistrationRepository,
-            final ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
 
-        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider
-                = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
                         .clientCredentials()
+                        .password()
                         .build();
 
-        DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager
-                = new DefaultReactiveOAuth2AuthorizedClientManager(clientRegistrationRepository,
-                        authorizedClientRepository);
-
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 
         return authorizedClientManager;
     }
 
     @Bean("kcAuthorizedWebClient")
-    public WebClient webClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+    public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
 
         String registrationId = "keycloak";
 
-        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth
-                = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
         oauth.setDefaultClientRegistrationId(registrationId);
 
         return WebClient.builder()
