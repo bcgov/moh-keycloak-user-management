8ab21c96190629ad104c5bce6eaaba111ec40999
Gustavo Lima <gustavo.lima@cgi.com>
2021-01-28 18:09:40 -0700

    Re:2247 :: New class used to select those user entities which contain a value of /attributes/org_details[*].id matching the Org. ID specified at construction time.

---
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/util/FilterUserByOrgId.java
@@ -0,0 +1,87 @@
+package ca.bc.gov.hlth.mohums.util;
+
+import com.nimbusds.oauth2.sdk.util.CollectionUtils;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Predicate;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+/**
+ *
+ */
+public final class FilterUserByOrgId implements Predicate<Object> {
+
+    private static final int ABBREVIATED_USER_ENTITY_LENGTH = 64;
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizedClientsParser.class);
+
+    private final String orgId;
+    private final JSONParser jsonParser;
+    
+    public FilterUserByOrgId(final String id) {
+        this.orgId = id;
+        this.jsonParser = new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE);
+    }
+    
+    Logger getLogger() {
+        return LOGGER;
+    }
+    
+    @Override
+    public boolean test(final Object userEntity) {
+        final Expression expression = new SpelExpressionParser()
+                .parseExpression("#this['attributes']['org_details']");
+        final EvaluationContext context = new StandardEvaluationContext(userEntity);
+
+        Collection<?> orgDetails = Collections.emptyList();
+
+        try {
+            orgDetails = expression.getValue(context, Collection.class);
+        } catch (final RuntimeException e) {
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug("Unable to filter \"{}\" by Org. ID {}: {}.",
+                        abbreviateEntity(userEntity), orgId, e.toString());
+            }
+        }
+
+        return CollectionUtils.isNotEmpty(orgDetails) && orgDetails
+                .stream()
+                .map(this::extractOrgId)
+                .anyMatch(this.orgId::equalsIgnoreCase);
+    }
+    
+    String extractOrgId(final Object orgDetails) {
+        String id = StringUtils.EMPTY; // Default value.
+
+        final String organizationDetails = Objects.toString(orgDetails, "{}");
+
+        try {
+            final JSONObject organization = (JSONObject) jsonParser.parse(organizationDetails);
+            id = organization.getOrDefault("id", id).toString();
+        } catch (final ParseException | RuntimeException e) {
+            getLogger().warn("Unable to parse: \"{}\": {}.", organizationDetails, e.toString());
+        }
+
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s(%s)", getClass().getSimpleName(), this.orgId);
+    }
+
+    private static String abbreviateEntity(final Object userEntity) {
+        return StringUtils.abbreviate(Objects.toString(userEntity), ABBREVIATED_USER_ENTITY_LENGTH);
+    }
+    
+}
