68897df22221bc7a739016b5236d6c1c3888b69d
Trevor Schiavone <trevor.schiavone@cgi.com>
2021-02-01 19:50:19 -0800

    initial commit of advanced search

--- frontend/src/components/UserSearch.vue
+++ frontend/src/components/UserSearch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-row no-gutters>
+    <v-row no-gutters v-if="!this.advancedSearchSelected">
       <v-col class="col-6">
         <label for="user-search">
           Search
@@ -12,6 +12,10 @@
           </v-tooltip>
         </label>
 
+        <a id="advancedSearchLink" style="float:right" v-on:click="advancedSearchSelected=true">
+          Advanced Search
+        </a>
+
         <v-text-field
           id="user-search"
           outlined
@@ -21,10 +25,98 @@
           @keyup.native.enter="searchUser"
         />
       </v-col>
-      <v-col class="col-1">
+      <v-col class="col-4">
         <v-btn id="search-button" class="secondary" medium @click.native="searchUser">Search Users</v-btn>
       </v-col>
+      <v-col class="col-2">
+        <v-btn id="create-user-button" class="success" medium @click.native="goToCreateUser">Create New User</v-btn>
+      </v-col>
     </v-row>
+
+    <!-- Advanced Search -->
+    <v-row class="right-gutters" v-if="this.advancedSearchSelected">
+      <v-col class="col-6">
+        <h1>Advanced User Search</h1>
+      </v-col>
+      <v-col class="col-6">
+        <v-btn id="adv-create-user-button" class="success" medium @click.native="goToCreateUser">Create New User</v-btn>
+      </v-col>
+
+      <v-col class="col-6">
+        <label for="adv-search-last-name">
+          Last Name
+        </label>
+        <v-text-field
+            id="adv-search-last-name"
+            outlined
+            dense
+            v-model="lastNameInput"
+        />
+      </v-col>
+      <v-col class="col-6">
+        <label for="adv-search-first-name">
+          First Name
+        </label>
+        <v-text-field
+            id="adv-search-first-name"
+            outlined
+            dense
+            v-model="firstNameInput"
+        />
+      </v-col>
+      <v-col class="col-6">
+        <label for="adv-search-username">
+          Username
+        </label>
+        <v-text-field
+            id="adv-search-username"
+            outlined
+            dense
+            v-model="usernameInput"
+        />
+      </v-col>
+      <v-col class="col-6">
+        <label for="adv-search-email">
+          Email
+        </label>
+        <v-text-field
+            id="adv-search-email"
+            outlined
+            dense
+            v-model="emailInput"
+        />
+      </v-col>
+      <v-col class="col-6">
+        <label for="adv-search-org">
+          Organization
+        </label>
+        <v-autocomplete
+            id="org-details"
+            v-model="organizationInput"
+            :items="organizations"
+            item-value="id"
+            dense
+            outlined
+        ></v-autocomplete>
+      </v-col>
+      <v-col class="col-6">
+        <label for="adv-search-role">
+          Role
+        </label>
+        <v-text-field
+            id="adv-search-role"
+            outlined
+            dense
+        />
+      </v-col>
+      <v-col class="col-4" style="margin-bottom: 30px">
+        <v-btn id="adv-search-button" class="secondary" medium @click.native="searchUserAdvanced">Search Users</v-btn>
+        <a id="basicSearchLink" style="margin-left: 10px" v-on:click="advancedSearchSelected=false">
+         Return to Basic Search
+        </a>
+      </v-col>
+    </v-row>
+
     <v-row no-gutters>
       <v-col class="col-12">
         <v-data-table
@@ -39,16 +131,16 @@
         ></v-data-table>
       </v-col>
     </v-row>
-    <v-btn id="create-user-button" class="secondary" medium @click.native="goToCreateUser">Create New User</v-btn>
+
   </div>
 </template>
 
 <script>
 import UsersRepository from "@/api/UsersRepository";
+import organizations from "@/assets/organizations"
 
 export default {
   name: "UserSearch",
-
   data() {
     return {
       headers: [
@@ -59,10 +151,17 @@ export default {
         { text: "Enabled", value: "enabled", class: "table-header" },
         { text: "Keycloak User ID", value: "id", class: "table-header" }
       ],
+      organizations: organizations,
       footerProps: { "items-per-page-options": [15] },
       userSearchInput: "",
+      lastNameInput: "",
+      firstNameInput: "",
+      usernameInput: "",
+      emailInput: "",
+      organizationInput: "",
       searchResults: [],
       userSearchLoadingStatus: false,
+      advancedSearchSelected: false
     };
   },
   methods: {
@@ -92,6 +191,35 @@ export default {
           window.scrollTo(0, 0);
         })
         .finally(() => (this.userSearchLoadingStatus = false));
+    },
+    searchUserAdvanced: function() {
+      let params = '';
+      params = this.addQueryParameter(params, "lastName", this.lastNameInput)
+      params = this.addQueryParameter(params, "firstName", this.firstNameInput)
+      params = this.addQueryParameter(params, "username", this.usernameInput)
+      params = this.addQueryParameter(params, "email", this.emailInput)
+      params = this.addQueryParameter(params, "org", this.organizationInput)
+
+      UsersRepository.get(
+          "?briefRepresentation=true&first=0&max=300" + params
+      )
+          .then(response => {
+            this.searchResults = response.data;
+          })
+          .catch(error => {
+            this.$store.commit("alert/setAlert", {
+              message: "User search failed: " + error,
+              type: "error"
+            });
+            window.scrollTo(0, 0);
+          })
+          .finally(() => (this.userSearchLoadingStatus = false));
+    },
+    addQueryParameter: function(parameters, parameter, value) {
+      if (value) {
+        parameters += "&" + parameter + "=" + value;
+      }
+      return parameters;
     }
   }
 };
@@ -101,5 +229,15 @@ export default {
 #search-button {
   margin-top: 25px;
   margin-left: 20px;
+}
+#create-user-button {
+  float: right;
+  margin-top: 25px;
+}
+#adv-create-user-button {
+  float: right;
+}
+.right-gutters .col {
+  padding: 0 24px 0 0;
 }
 </style>
