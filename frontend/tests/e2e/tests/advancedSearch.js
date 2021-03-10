81565f336298222078f66314873b34993a0792f7
Gustavo Lima <gustavo.lima@cgi.com>
2021-03-10 16:19:03 -0700

    Re:2665 :: Added test for search against a Big Role (one containing more than max_results users).

--- frontend/tests/e2e/tests/advancedSearch.js
+++ frontend/tests/e2e/tests/advancedSearch.js
@@ -1,4 +1,5 @@
 import { itsbAccessTeamUser } from "../roles/roles"
+import AlertPage from "../pages/AlertPage";
 import AdvancedSearchForm from "../pages/AdvancedSearchForm";
 
 const SITE_UNDER_TEST = 'http://localhost:8080/#/users/';
@@ -93,4 +94,20 @@ test('Test adv search by Email and Role, then Basic Search', async t => {
         .click('#search-button')
         .expect(AdvancedSearchForm.getResultsCount())
         .gt(1);
+});
+
+// Run MoHUmsPrepareLoadTests.createDesiredUserCount() before this test
+// and make sure that desiredUserCount is set to a more than the value
+// of "max_results" defined in /public/config.json.
+test('Test adv search by Big Role', async t => {
+    await t
+        .click(AdvancedSearchForm.advSearchLink)
+        .typeText(AdvancedSearchForm.selectClient, 'LOADTEST')
+        .pressKey('enter')
+        .click(AdvancedSearchForm.getRoleCheckbox('LOAD_TEST'))
+        .click(AdvancedSearchForm.advSearchButton)
+        .expect(AdvancedSearchForm.getResultsCount())
+        .gt(10)
+        .expect(AlertPage.alertBannerText.textContent).contains(
+          "Your search returned more than the maximum number of results");
 });
