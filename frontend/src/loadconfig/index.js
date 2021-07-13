d92e6691886248c80e2e5a523773701537192a94
Camille <camille.estival@cgi.com>
2021-07-13 11:46:56 -0700

    fix bugs in testCafe tests with userID

--- frontend/src/loadconfig/index.js
+++ frontend/src/loadconfig/index.js
@@ -1,5 +1,7 @@
+//import fetch from 'cross-fetch';
 let app_config = new Object();
 
+//const fetch = require("node-fetch");
 fetch(process.env.BASE_URL + "config.json")
     .then((response) => {
         return response.json();
--- frontend/tests/e2e/alltests.js
+++ frontend/tests/e2e/alltests.js
@@ -4,6 +4,7 @@ import { Role } from 'testcafe';
 const SITE_UNDER_TEST = 'http://localhost:8080';
 
 const TEST_CAFE_USER_ID = '3195a1bf-4bea-47c4-955d-cf52d4e2fc15';
+const TEST_CAFE_USERNAME = 'testcafe';
 const CLIENT_TO_TEST = 'FMDB';
 
 const regularAccUser = Role(SITE_UNDER_TEST, async t => {
@@ -33,7 +34,9 @@ test('Test search', async t => {
         .click('#search-button')
         .expect(Selector('html').textContent)
         // This is the ID of the testcafe user.
-        .contains(TEST_CAFE_USER_ID);
+        // TODO Replace ID with Username
+//        .contains(TEST_CAFE_USER_ID);
+        .contains(TEST_CAFE_USERNAME);
 });
 
 test('Test update user', async t => {
@@ -43,7 +46,9 @@ test('Test update user', async t => {
         .typeText('#user-search', 'testcafe')
         .click('#search-button')
         // This is the ID of the testcafe user.
-        .click(Selector('td').withText(TEST_CAFE_USER_ID))
+        // TODO Replace ID with Username
+//        .click(Selector('td').withText(TEST_CAFE_USER_ID))
+        .click(Selector('td').withText(TEST_CAFE_USERNAME))
         .typeText('#org-details', random_value, { replace: true })
         .click('#submit-button')
         .expect(Selector('html').textContent)
@@ -53,8 +58,10 @@ test('Test update user', async t => {
 test('Test update user role', async t => {
     await t
         .typeText('#user-search', 'testcafe')
-        .click('#search-button')
-        .click(Selector('td').withText(TEST_CAFE_USER_ID))
+        .click('#search-button')        
+// TODO Replace ID with Username
+//        .click(Selector('td').withText(TEST_CAFE_USER_ID))
+        .click(Selector('td').withText(TEST_CAFE_USERNAME))
         .typeText('#select-client', CLIENT_TO_TEST, { replace: true })
         .click(Selector('.v-list-item').withText(CLIENT_TO_TEST))
         .click('#role-0')
@@ -66,7 +73,9 @@ test('Test update user role', async t => {
 test('Test search by administrator', async t => {
     await t
         .click('#admin-event-log-link')
-        .typeText('#admin-id', TEST_CAFE_USER_ID)
+        // TODO Replace ID with Username => TO TEST
+      .typeText('#admin-id', TEST_CAFE_USER_ID)
+//        .typeText('#admin-id', TEST_CAFE_USERNAME)
         .pressKey('enter')
         .expect(Selector('#search-results').textContent)
         .contains('Test Cafe');
