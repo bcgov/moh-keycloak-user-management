89cc527d839b20e592663028835359f51a853afa
vangeliq <valeryangelique2001@gmail.com>
2022-03-03 15:13:41 -0800

    connected backend to a postgres database
    connected backend to database

--- backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
@@ -1,61 +1,85 @@
 package ca.bc.gov.hlth.mohums.controller;
 
 import ca.bc.gov.hlth.mohums.webclient.WebClientService;
+import io.netty.util.internal.SocketUtils;
 import net.minidev.json.JSONObject;
 
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
+
+
 
 @RestController
 public class OrganizationsController {
-    private Map<String, JSONObject> organizations;
+
+    //todo: write the url, user and password somewhere else
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.user}")
+    private String user;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.database}")
+    private String databaseName;
 
     public OrganizationsController(WebClientService webClientService, @Value("${config.vanity-hostname}") String vanityHostname) {
-        organizations = new HashMap<>();
+    }
 
-        organizations.put("00000010", new JSONObject(Map.of("id", "00000010", "name", "Ministry of Health")));
-        organizations.put("00000025", new JSONObject(Map.of("id", "00000025", "name", "Government of Yukon - Dept of Health")));
-        organizations.put("00000032", new JSONObject(Map.of("id", "00000032", "name", "Providence Health Care")));
-        organizations.put("00001480", new JSONObject(Map.of("id", "00001480", "name", "TimberWest Forest Company")));
-        organizations.put("00001762", new JSONObject(Map.of("id", "00001762", "name", "Northern Health Authority")));
-        organizations.put("00001763", new JSONObject(Map.of("id", "00001763", "name", "Interior Health Authority")));
-        organizations.put("00001764", new JSONObject(Map.of("id", "00001764", "name", "Vancouver Island Health Authority")));
-        organizations.put("00001765", new JSONObject(Map.of("id", "00001765", "name", "Vancouver Coastal Health Authority")));
-        organizations.put("00001766", new JSONObject(Map.of("id", "00001766", "name", "Fraser Health Authority")));
-        organizations.put("00001767", new JSONObject(Map.of("id", "00001767", "name", "Provincial Health Services Authority")));
-        organizations.put("00002158", new JSONObject(Map.of("id", "00002158", "name", "Provider Registry Project")));
-        organizations.put("00005502", new JSONObject(Map.of("id", "00005502", "name", "ICBC - Head Office")));
-        organizations.put("00006501", new JSONObject(Map.of("id", "00006501", "name", "Belkorp Industries Inc - Parent")));
-        organizations.put("00015824", new JSONObject(Map.of("id", "00015824", "name", "First Nations Health Authority - Employees")));
+    public PreparedStatement connectAndPrepareStatement(String sql) throws SQLException{
+        return DriverManager.getConnection(url, user, password).prepareStatement(sql);
     }
 
     @GetMapping("/organizations")
     public Collection<JSONObject> getOrganizations() {
-        return organizations.values();
+        Collection<JSONObject> result = new LinkedList<JSONObject>();
+        String SQL = "SELECT * FROM " + databaseName;
+
+        try {
+            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
+            ResultSet rs = pstmt.executeQuery();
+        
+            while (rs.next()) {
+                String id = rs.getString("organization_id");
+                String name = rs.getString("organization_name");
+                result.add(new JSONObject(Map.of("id", id, "name", name)));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 
-
     @GetMapping("/organizations/{organizationId}")
-    public JSONObject getOrganization(@PathVariable String organizationId) {
-        return organizations.get(organizationId);
+    public JSONObject getOrganization(@PathVariable String organizationId) throws SQLException {
+        String SQL = "SELECT * FROM " + databaseName;
+
+            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();    
+
+            String id = rs.getString("organization_id");
+            String name = rs.getString("organization_name");
+            return new JSONObject(Map.of("id", id, "name", name));
     }
 
 
     @PostMapping("/organizations")
-    public void createOrganization(@RequestBody JSONObject body) {
-        String id = (String) body.get("id");
+    public void createOrganization(@RequestBody JSONObject body) throws SQLException {
+        String SQL = "INSERT INTO organizations(organization_id,organization_name) "
+                + "VALUES(?,?)";
 
-        // todo: error handling
-        if (organizations.containsKey(id)) {
-            System.out.println("ERROR HANDLING HERE: an org with this name already exists");
-        } else {
-            organizations.put(id, body);
-        }
+            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
+
+            pstmt.setString(1, (String) body.get("id"));
+            pstmt.setString(2, (String) body.get("name"));
+
+            pstmt.executeUpdate();
     }
 
 }
