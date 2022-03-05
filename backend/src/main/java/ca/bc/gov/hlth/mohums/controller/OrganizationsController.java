ef9b7256b750607f06fd3cc7b311f846f395af1b
vangeliq <valeryangelique2001@gmail.com>
2022-03-04 16:13:21 -0800

    used the springboot JDBC

--- backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
@@ -1,18 +1,17 @@
 package ca.bc.gov.hlth.mohums.controller;
 
 import ca.bc.gov.hlth.mohums.webclient.WebClientService;
-import io.netty.util.internal.SocketUtils;
 import net.minidev.json.JSONObject;
-
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.*;
-
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Map;
 
 
 @RestController
@@ -21,65 +20,40 @@ public class OrganizationsController {
     //todo: write the url, user and password somewhere else
     @Value("${spring.datasource.url}")
     private String url;
-    @Value("${spring.datasource.user}")
+    @Value("${spring.datasource.username}")
     private String user;
     @Value("${spring.datasource.password}")
     private String password;
-    @Value("${spring.datasource.database}")
-    private String databaseName;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public OrganizationsController(WebClientService webClientService, @Value("${config.vanity-hostname}") String vanityHostname) {
     }
 
-    public PreparedStatement connectAndPrepareStatement(String sql) throws SQLException{
+    public PreparedStatement connectAndPrepareStatement(String sql) throws SQLException {
         return DriverManager.getConnection(url, user, password).prepareStatement(sql);
     }
 
     @GetMapping("/organizations")
-    public Collection<JSONObject> getOrganizations() {
-        Collection<JSONObject> result = new LinkedList<JSONObject>();
-        String SQL = "SELECT * FROM " + databaseName;
-
-        try {
-            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
-            ResultSet rs = pstmt.executeQuery();
-        
-            while (rs.next()) {
-                String id = rs.getString("organization_id");
-                String name = rs.getString("organization_name");
-                result.add(new JSONObject(Map.of("id", id, "name", name)));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return result;
+    public Collection<Object> getOrganizations() throws SQLException {
+        return jdbcTemplate.query("SELECT * FROM organizations ORDER BY organization_id",
+                (rs, rowNum) -> Map.of("id", rs.getString("organization_id"),
+                        "name", rs.getString("organization_name")));
     }
 
     @GetMapping("/organizations/{organizationId}")
-    public JSONObject getOrganization(@PathVariable String organizationId) throws SQLException {
-        String SQL = "SELECT * FROM " + databaseName;
-
-            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
-            ResultSet rs = pstmt.executeQuery();
-            rs.next();    
-
-            String id = rs.getString("organization_id");
-            String name = rs.getString("organization_name");
-            return new JSONObject(Map.of("id", id, "name", name));
+    public Object getOrganization(@PathVariable String organizationId) throws SQLException {
+        return jdbcTemplate.queryForObject("SELECT * FROM organizations WHERE organization_id = ?", new Object[]{organizationId},
+                (rs, rowNum) -> Map.of("id", rs.getString("organization_id"), "name", rs.getString("organization_name"))
+        );
     }
 
 
     @PostMapping("/organizations")
     public void createOrganization(@RequestBody JSONObject body) throws SQLException {
-        String SQL = "INSERT INTO organizations(organization_id,organization_name) "
-                + "VALUES(?,?)";
-
-            PreparedStatement pstmt = this.connectAndPrepareStatement(SQL);
-
-            pstmt.setString(1, (String) body.get("id"));
-            pstmt.setString(2, (String) body.get("name"));
-
-            pstmt.executeUpdate();
+        jdbcTemplate.update("insert into organizations (organization_id, organization_name) values (?,?)",
+                body.get("id"), body.get("name"));
     }
 
 }
