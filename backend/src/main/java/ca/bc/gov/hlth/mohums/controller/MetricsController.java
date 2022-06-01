a548b29edfe0fe6402a394700b105ff2ed4b48a2
ahoplock <68440291+ahoplock@users.noreply.github.com>
2022-05-31 20:24:44 -0500

    AB#6567: Update metrics queries to match latest from Confluence's Keycloak User Counts Extract; Fix for test timeouts; Formatting

--- backend/src/main/java/ca/bc/gov/hlth/mohums/controller/MetricsController.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/controller/MetricsController.java
@@ -27,28 +27,79 @@ public class MetricsController {
 
     @GetMapping("/metrics/active-user-count")
     public List<Map<String, Object>> getActiveUserCount() throws SQLException {
-        String sql = "SELECT realm_id AS REALM, client_id AS CLIENT, COUNT(1) AS ACTIVE_USER_COUNT FROM ( SELECT DISTINCT ue.realm_id, c.client_id, ue.username FROM keycloak.event_entity ee INNER JOIN keycloak.user_entity ue ON ue.id = ee.user_id INNER JOIN keycloak.client c ON c.client_id = ee.client_id INNER JOIN keycloak.keycloak_role kcr ON kcr.client = c.id INNER JOIN keycloak.user_role_mapping urm ON urm.user_id = ee.user_id AND urm.role_id = kcr.id WHERE ee.type = 'LOGIN' AND ee.event_time > (SYSDATE-365-TO_DATE('1970-01-01','YYYY-MM-DD'))*24*60*60*1000 AND ue.enabled = 1 UNION SELECT DISTINCT ue.realm_id, c.client_id, ue.username FROM keycloak.event_entity ee INNER JOIN keycloak.user_entity ue ON ue.id = ee.user_id INNER JOIN keycloak.client c ON c.client_id = ee.client_id LEFT OUTER JOIN keycloak.keycloak_role kcr ON kcr.client = c.id WHERE ee.type = 'LOGIN' AND ee.event_time > (SYSDATE-365-TO_DATE('1970-01-01','YYYY-MM-DD'))*24*60*60*1000 AND ue.enabled = 1  AND kcr.id IS NULL ) WHERE NOT (client_id = 'account' AND (realm_id IN ('bceid_basic', 'bceid_business', 'idir', 'idir_aad', 'master', 'moh_applications', 'moh_citizen', 'phsa')) OR (client_id IN ('realm-management', 'USER-MANAGEMENT-SERVICE', 'PRIME-WEBAPP-ENROLLMENT'))) GROUP BY realm_id, client_id  ORDER BY realm_id ASC, client_id ASC";
+        String sql
+                = "SELECT realm_id AS REALM, client_id AS CLIENT, COUNT(1) AS ACTIVE_USER_COUNT\n"
+                + "  FROM (\n"
+                + "    SELECT DISTINCT ue.realm_id, c.client_id, ue.username\n"
+                + "      FROM keycloak.event_entity ee\n"
+                + "     INNER JOIN keycloak.user_entity ue ON ue.id = ee.user_id\n"
+                + "     INNER JOIN keycloak.client c ON c.client_id = ee.client_id\n"
+                + "     INNER JOIN keycloak.keycloak_role kcr ON kcr.client = c.id\n"
+                + "     INNER JOIN keycloak.user_role_mapping urm ON urm.user_id = ee.user_id AND urm.role_id = kcr.id\n"
+                + "     WHERE ee.type = 'LOGIN'\n"
+                + "       AND ee.event_time > (SYSDATE-365-TO_DATE('1970-01-01','YYYY-MM-DD'))*24*60*60*1000\n"
+                + "       AND ue.enabled = 1\n"
+                + "     UNION\n"
+                + "    SELECT DISTINCT ue.realm_id, c.client_id, ue.username\n"
+                + "      FROM keycloak.event_entity ee\n"
+                + "     INNER JOIN keycloak.user_entity ue ON ue.id = ee.user_id\n"
+                + "     INNER JOIN keycloak.client c ON c.client_id = ee.client_id\n"
+                + "      LEFT OUTER JOIN keycloak.keycloak_role kcr ON kcr.client = c.id\n"
+                + "     WHERE ee.type = 'LOGIN'\n"
+                + "       AND ee.event_time > (SYSDATE-365-TO_DATE('1970-01-01','YYYY-MM-DD'))*24*60*60*1000\n"
+                + "       AND ue.enabled = 1\n"
+                + "       AND kcr.id IS NULL\n"
+                + " )\n"
+                + " WHERE NOT (client_id = 'account' AND (LOWER(realm_id) IN ('bceid_basic', 'bceid_business', 'bcprovider_aad', 'bcsc', 'idir', 'idir_aad', 'master', 'moh_applications', 'moh_citizen', 'moh_idp', 'phsa'))\n"
+                + "          OR (client_id IN ('realm-management', 'USER-MANAGEMENT-SERVICE', 'PRIME-WEBAPP-ENROLLMENT')))\n"
+                + " GROUP BY realm_id, client_id\n"
+                + " ORDER BY realm_id ASC, client_id ASC";
 
         return jdbcTemplate.queryForList(sql);
     }
 
     @GetMapping("/metrics/total-number-of-users")
     public Object getTotalNumberOfUsers() throws SQLException {
-        String sql = "SELECT COUNT(1) AS TOTAL_USER_COUNT FROM (SELECT DISTINCT id, realm_id FROM keycloak.user_entity WHERE (realm_id IN ('bceid_basic', 'bceid_business', 'idir', 'phsa')) OR (realm_id = 'idir_aad' AND username NOT IN (SELECT username FROM keycloak.user_entity WHERE realm_id = 'idir')) AND enabled = 1)";
+        String sql
+                = "SELECT COUNT(1) AS TOTAL_USER_COUNT\n"
+                + "  FROM (\n"
+                + "    SELECT DISTINCT id, realm_id\n"
+                + "      FROM keycloak.user_entity\n"
+                + "     WHERE (LOWER(realm_id) IN ('bceid_basic', 'bceid_business', 'bcprovider_aad', 'bcsc', 'idir', 'moh_idp', 'phsa')) OR (realm_id = 'idir_aad' AND username NOT IN (SELECT username FROM keycloak.user_entity WHERE realm_id = 'idir'))\n"
+                + "       AND enabled = 1\n"
+                + " )";
 
         return jdbcTemplate.queryForList(sql).get(0).get("TOTAL_USER_COUNT");
     }
 
     @GetMapping("/metrics/unique-user-count-by-idp")
     public List<Map<String, Object>> getUniqueUserCountByIDP() throws SQLException {
-        String sql = " SELECT realm_id AS IDP, COUNT(1) AS UNIQUE_USER_COUNT FROM ( SELECT DISTINCT id, realm_id FROM keycloak.user_entity WHERE realm_id IN ('bceid_basic', 'bceid_business', 'idir', 'idir_aad', 'phsa') AND enabled = 1 ) GROUP BY realm_id ORDER BY realm_id ASC";
+        String sql
+                = "SELECT realm_id AS IDP, COUNT(1) AS UNIQUE_USER_COUNT\n"
+                + "  FROM (\n"
+                + "    SELECT DISTINCT id, realm_id\n"
+                + "      FROM keycloak.user_entity\n"
+                + "     WHERE LOWER(realm_id) IN ('bceid_basic', 'bceid_business', 'bcprovider_aad', 'bcsc', 'idir', 'idir_aad', 'moh_idp', 'phsa')\n"
+                + "       AND enabled = 1\n"
+                + " )\n"
+                + " GROUP BY realm_id\n"
+                + " ORDER BY realm_id ASC";
 
         return jdbcTemplate.queryForList(sql);
     }
 
     @GetMapping("/metrics/unique-user-count-by-realm")
     public List<Map<String, Object>> getUniqueUserCountByRealm() throws SQLException {
-        String sql = "SELECT realm_id AS REALM, COUNT(1) AS UNIQUE_USER_COUNT FROM ( SELECT DISTINCT id, realm_id FROM keycloak.user_entity WHERE realm_id NOT IN ('bceid_basic', 'bceid_business', 'idir', 'idir_aad', 'master', 'phsa') AND enabled = 1 ) GROUP BY realm_id ORDER BY realm_id ASC";
+        String sql
+                = "SELECT realm_id AS REALM, COUNT(1) AS UNIQUE_USER_COUNT\n"
+                + "  FROM (\n"
+                + "    SELECT DISTINCT id, realm_id\n"
+                + "      FROM keycloak.user_entity\n"
+                + "     WHERE LOWER(realm_id) NOT IN ('bceid_basic', 'bceid_business', 'bcprovider_aad', 'bcsc', 'idir', 'idir_aad', 'master', 'moh_idp', 'phsa')\n"
+                + "       AND enabled = 1\n"
+                + " )\n"
+                + " GROUP BY realm_id\n"
+                + " ORDER BY realm_id ASC";
 
         return jdbcTemplate.queryForList(sql);
     }
