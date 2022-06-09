7bfca4c0bbabb75d97c32f7f32d1038580f6892d
ahoplock <68440291+ahoplock@users.noreply.github.com>
2022-06-09 13:11:31 -0500

    AB#6418: ...another attempt

--- frontend/src/loadconfig/index.js
+++ frontend/src/loadconfig/index.js
@@ -1,15 +1,33 @@
-let app_config = { config: '', organizations: '[ { "id" : "", "name" :  "" } ]' };
+let app_config = {};
+let config_url = process.env.BASE_URL + "config.json";
+let organizations_url = process.env.BASE_URL + "organizations.json";
 
-fetch(process.env.BASE_URL + "config.json")
+fetch(config_url)
     .then(async response => {
+        // Firefox does not always return 200 OK on the first attempt
+        if (!response.ok) {
+            fetch(config_url)
+                .then(async response => {
+                    console.log("Firefox was here");
+                    return await response.json();
+                });
+        }
         return await response.json();
     })
     .then((config) => {
         app_config.config = config;
     });
 
-fetch(process.env.BASE_URL + "organizations.json")
+fetch(organizations_url)
     .then(async response => {
+        // Firefox does not always return 200 OK on the first attempt
+        if (!response.ok) {
+            fetch(organizations_url)
+                .then(async response => {
+                    console.log("Firefox was here");
+                    return await response.json();
+                });
+        }
         return await response.json();
     })
     .then((organizations) => {
