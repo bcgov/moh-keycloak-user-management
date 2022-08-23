afae898edc62a7370a5430969f73e4b085a6c482
Florek <filip.florek@cgi.com>
2022-08-23 16:06:17 -0600

    create and handle error pages

---
+++ frontend/src/views/AccessDenied.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    <center>
+            <img src="../assets/images/accessDenied.png" width="300" class="logo" alt="403" />
+            <h3>Access denied. You don't have permissions to view this page</h3>
+    </center>
+</div>
+</template>
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    <center>
+            <img src="../assets/images/notFound.png" width="300" class="logo" alt="404" />
+            <h3>Page Not Found</h3>
+    </center>
+</div>
+</template>
