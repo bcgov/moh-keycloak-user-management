69848839de5e328a17da22376a4991899e84ad78
vangeliq <valeryangelique2001@gmail.com>
2022-08-31 11:26:44 -0700

    formatted the file

--- frontend/src/components/PieChart.vue
+++ frontend/src/components/PieChart.vue
@@ -1,60 +1,83 @@
 <template>
-  <div class="pieChart" 
-  style="
-  background-color:white;
-
-  ">
-      <Pie
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-         />
-    </div>
+  <div class="pieChart" style="background-color: white">
+    <Pie :chart-options="chartOptions" :chart-data="chartData" />
+  </div>
 </template>
 
 <script>
-import { Pie } from 'vue-chartjs/legacy'
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale,  ArcElement,
- LinearScale } from 'chart.js'
+import { Pie } from "vue-chartjs/legacy";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  ArcElement,
+  LinearScale,
+} from "chart.js";
 
-ChartJS.register(ChartDataLabels);
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, ArcElement,LinearScale)
+ChartJS.register(
+  ChartDataLabels,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  ArcElement,
+  LinearScale
+);
 
 export default {
-  name: 'PieChart',
+  name: "PieChart",
   components: { Pie },
   props: {
     pieChartData: {
       type: Object,
-      Default: {     
-       }
+      Default: {},
     },
   },
   data() {
-
     return {
       chartData: {
         labels: this.pieChartData["labels"],
-        datasets: [ {
-          backgroundColor:['#003366',"#0C5297", '#C7DEF5', '#1F66AD', '#86bdd9','#205e7e','#4b82a1','#3A80A6','#9ad1ec'],
-          data: this.pieChartData["UNIQUE_USER_COUNT"],       
-          datalabels: {
-            align: "start",
-            offset: -20,
-            anchor: function(context) {
-              const sum = context.dataset.data.reduce((sum, current) => sum + current);
-              const value = 100 * context.dataset.data[context.dataIndex] /sum;
-              return value < 6 ? 'end' : 
-              'center'
+        datasets: [
+          {
+            backgroundColor: [
+              "#003366",
+              "#0C5297",
+              "#C7DEF5",
+              "#1F66AD",
+              "#86bdd9",
+              "#205e7e",
+              "#4b82a1",
+              "#3A80A6",
+              "#9ad1ec",
+            ],
+            data: this.pieChartData["UNIQUE_USER_COUNT"],
+            datalabels: {
+              align: "start",
+              offset: -20,
+              anchor: function (context) {
+                const sum = context.dataset.data.reduce(
+                  (sum, current) => sum + current
+                );
+                const value =
+                  (100 * context.dataset.data[context.dataIndex]) / sum;
+                return value < 6 ? "end" : "center";
+              },
+              color: function (context) {
+                const sum = context.dataset.data.reduce(
+                  (sum, current) => sum + current
+                );
+                const value =
+                  (100 * context.dataset.data[context.dataIndex]) / sum;
+                return value < 6 ? "#003366" : "#f7f7f7";
+              },
             },
-            color: function(context) {
-              const sum = context.dataset.data.reduce((sum, current) => sum + current);
-              const value = 100 * context.dataset.data[context.dataIndex] /sum;
-              return value < 6 ? '#003366' :
-              '#f7f7f7'    
-            },
-          }      
-        }]
+          },
+        ],
       },
       chartOptions: {
         responsive: true,
@@ -62,40 +85,39 @@ export default {
         aspectRatio: 1.5,
         radius: 60,
         plugins: {
-          align:"start",
+          align: "start",
           tooltip: {
             displayColors: false,
-            position:'nearest',
+            position: "nearest",
             padding: 3,
             bodyFont: {
-                size: 10
-            }
+              size: 10,
+            },
           },
           legend: {
-            align:"center",
+            align: "center",
             position: "right",
             labels: {
-              boxWidth:10,
-              boxHeight:10,
+              boxWidth: 10,
+              boxHeight: 10,
               font: {
-                size:9
-              }
+                size: 9,
+              },
             },
           },
         },
         layout: {
           padding: {
-            top:0,
-            right:5,
-            bottom:0,
-            left:5,
-          }
-        }
-      }
-    }
-    
+            top: 0,
+            right: 5,
+            bottom: 0,
+            left: 5,
+          },
+        },
+      },
+    };
   },
-}
+};
 </script>
 
 <style scoped>
