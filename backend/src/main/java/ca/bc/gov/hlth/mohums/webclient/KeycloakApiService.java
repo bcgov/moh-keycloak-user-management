cc4cfa479edd8a51169628c19a2ceafeeb9e3a41
Florek <filip.florek@cgi.com>
2022-09-30 14:32:47 -0600

    WebClientService refactor

--- backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/KeycloakApiService.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/KeycloakApiService.java
@@ -5,7 +5,6 @@ import ca.bc.gov.hlth.mohums.model.GroupDescriptionGenerator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
