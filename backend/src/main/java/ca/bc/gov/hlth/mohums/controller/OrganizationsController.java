5aec3d975a0801fcd5e374a88d535ac357abaf19
Florek <filip.florek@cgi.com>
2022-10-13 13:18:35 -0600

    add organization management endpoints and integration tests

--- backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/controller/OrganizationsController.java
@@ -3,23 +3,37 @@ package ca.bc.gov.hlth.mohums.controller;
 
 import ca.bc.gov.hlth.mohums.webclient.OrganizationsApiService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class OrganizationsController {
-
-    private final OrganizationsApiService webClientService;
     
-    public OrganizationsController(OrganizationsApiService webClientService) {
-        this.webClientService = webClientService;
+    private final OrganizationsApiService organizationsApiService;
+
+    public OrganizationsController(OrganizationsApiService organizationsApiService) {
+        this.organizationsApiService = organizationsApiService;
     }
 
     @GetMapping("/organizations")
     public ResponseEntity<List<Object>> getOrganizations() {
-        return webClientService.getOrganizations();
+        return organizationsApiService.getOrganizations();
+    }
+
+    @GetMapping("/organizations/{organizationId}")
+    public ResponseEntity<Object> getOrganizationById(@PathVariable String organizationId) {
+        return organizationsApiService.getOrganization(organizationId);
+    }
+
+    @PostMapping("/organizations")
+    public ResponseEntity<Object> addOrganization(@RequestBody Object newOrganization) {
+        return organizationsApiService.addOrganization(newOrganization);
+    }
+
+    @PutMapping("/organizations/{organizationId}")
+    public ResponseEntity<Object> updateOrganization(@PathVariable String organizationId, @RequestBody Object organizationToUpdate) {
+        return organizationsApiService.updateOrganization(organizationId, organizationToUpdate);
     }
 
 
--- backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/OrganizationsApiService.java
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/webclient/OrganizationsApiService.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class OrganizationsApiService {
 
@@ -17,5 +18,18 @@ public class OrganizationsApiService {
 
     public ResponseEntity<List<Object>> getOrganizations() {
         return externalApiCaller.getList(organizationsPath);
+    }
+
+    public ResponseEntity<Object> getOrganization(String organizationId) {
+        return externalApiCaller.get(organizationsPath + "/" + organizationId, null);
+    }
+
+    public ResponseEntity<Object> addOrganization(Object newOrganization) {
+        return externalApiCaller.post(organizationsPath, newOrganization);
+    }
+
+    public ResponseEntity<Object> updateOrganization(String organizationId, Object organizationToUpdate) {
+        //make sure both org ids are the same
+        return externalApiCaller.put(organizationsPath + "/" + organizationId, organizationToUpdate);
     }
 }
