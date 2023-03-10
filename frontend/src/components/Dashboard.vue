d5a04e9cdcb5a7468862789373a60d385074bf9d
Alexandre Tremblay <125399080+alex-tremblay-cgi@users.noreply.github.com>
2023-03-10 11:31:27 -0500

    Remove duplicate button and format with prettier

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -6,11 +6,23 @@
           <div class="heading">
             <p>Active User Count (Login Event Within 365 Days)</p>
           </div>
-          <v-data-table id="Active-user-count-table" class="tile-table" :items="activeUserCount"
-            :headers="headerActiveUserCount" hide-default-footer dense no-data-text=""
-            :loading="activeUserCountLoadingStatus" :items-per-page="-1">
+          <v-data-table
+            id="Active-user-count-table"
+            class="tile-table"
+            :items="activeUserCount"
+            :headers="headerActiveUserCount"
+            hide-default-footer
+            dense
+            no-data-text=""
+            :loading="activeUserCountLoadingStatus"
+            :items-per-page="-1"
+          >
             <template #item.REALM="{ item }">
-              <v-tooltip bottom :disabled="!item.REALM_DESCRIPTION" max-width="300px">
+              <v-tooltip
+                bottom
+                :disabled="!item.REALM_DESCRIPTION"
+                max-width="300px"
+              >
                 <template v-slot:activator="{ on }">
                   <span v-on="on">{{ item.REALM }}</span>
                 </template>
@@ -36,11 +48,19 @@
               <template v-slot:activator="{ on }">
                 <v-icon v-on="on" small>mdi-help-circle</v-icon>
               </template>
-              <p class="tooltip">Total Unique User Count by IDP + MHSU Realms that do not use an IDP</p>
+              <p class="tooltip">
+                Total Unique User Count by IDP + MHSU Realms that do not use an
+                IDP
+              </p>
             </v-tooltip>
           </div>
           <p class="single-stat">{{ totalNumberOfUsers }}</p>
-          <v-skeleton-loader v-if="totalNumberOfUsersLoadingStatus" ref="dashboardSkeleton" type="text" max-width="60px">
+          <v-skeleton-loader
+            v-if="totalNumberOfUsersLoadingStatus"
+            ref="dashboardSkeleton"
+            type="text"
+            max-width="60px"
+          >
           </v-skeleton-loader>
         </div>
 
@@ -48,8 +68,16 @@
           <div class="heading">
             <p>Unique User Count (By IDP)</p>
           </div>
-          <PieChart :pieChartData="uniqueUserCountByIDP" v-if="!uniqueUserCountByIDPLoadingStatus" />
-          <v-skeleton-loader v-if="uniqueUserCountByIDPLoadingStatus" ref="dashboardSkeleton" width='' type="image">
+          <PieChart
+            :pieChartData="uniqueUserCountByIDP"
+            v-if="!uniqueUserCountByIDPLoadingStatus"
+          />
+          <v-skeleton-loader
+            v-if="uniqueUserCountByIDPLoadingStatus"
+            ref="dashboardSkeleton"
+            width=""
+            type="image"
+          >
           </v-skeleton-loader>
         </div>
 
@@ -57,12 +85,19 @@
           <div class="heading">
             <p>Unique User Count (By Realm)</p>
           </div>
-          <PieChart :pieChartData="uniqueUserCountByRealm" v-if="!uniqueUserCountByRealmLoadingStatus" />
-          <v-skeleton-loader v-if="uniqueUserCountByIDPLoadingStatus" ref="dashboardSkeleton" width='' type="image">
+          <PieChart
+            :pieChartData="uniqueUserCountByRealm"
+            v-if="!uniqueUserCountByRealmLoadingStatus"
+          />
+          <v-skeleton-loader
+            v-if="uniqueUserCountByIDPLoadingStatus"
+            ref="dashboardSkeleton"
+            width=""
+            type="image"
+          >
           </v-skeleton-loader>
         </div>
       </div>
-
     </div>
     <div class="flex">
       <div class="tile" style="width: 1100px">
@@ -70,29 +105,38 @@
           <p>Active Total User Count</p>
         </div>
         <div class="btn-group">
-          <v-btn class="btn" v-bind:class="getActiveClass('7D')" small rounded
-            @click="loadActiveTotalUser('7D')">7D</v-btn>
-          <v-btn class="btn" v-bind:class="getActiveClass('1M')" small rounded
-            @click="loadActiveTotalUser('1M')">1M</v-btn>
-          <v-btn class="btn" v-bind:class="getActiveClass('6M')" small rounded
-            @click="loadActiveTotalUser('6M')">6M</v-btn>
-          <v-btn class="btn" v-bind:class="getActiveClass('1Y')" small rounded
-            @click="loadActiveTotalUser('1Y')">1Y</v-btn>
+          <v-btn
+            v-for="format in ['7D', '1M', '6M', '1Y']"
+            :key="format"
+            class="btn"
+            v-bind:class="getActiveClass(format)"
+            small
+            rounded
+            @click="loadActiveTotalUser(format)"
+            >{{ format }}</v-btn
+          >
         </div>
-        <LineChart :lineChartData="totalUserCount" v-if="!totalUserCountLoadingStatus" />
-        <v-skeleton-loader v-if="totalUserCountLoadingStatus" ref="dashboardSkeleton" width='' type="image">
+        <LineChart
+          :lineChartData="totalUserCount"
+          v-if="!totalUserCountLoadingStatus"
+        />
+        <v-skeleton-loader
+          v-if="totalUserCountLoadingStatus"
+          ref="dashboardSkeleton"
+          width=""
+          type="image"
+        >
         </v-skeleton-loader>
       </div>
     </div>
   </div>
 </template>
 
-
 <script>
 import MetricsRepository from "@/api/MetricsRepository";
 import RealmsRepository from "@/api/RealmsRepository";
-import PieChart from '@/components/PieChart';
-import LineChart from '@/components/LineChart';
+import PieChart from "@/components/PieChart";
+import LineChart from "@/components/LineChart";
 
 export default {
   components: { PieChart, LineChart },
@@ -120,13 +164,13 @@ export default {
       uniqueUserCountByIDP: {},
       uniqueUserCountByRealm: {},
       totalUserCount: {},
-      totalUserCountSelectedFormat: '7D',
+      totalUserCountSelectedFormat: "7D",
       realmsInfo: [],
       totalNumberOfUsersLoadingStatus: true,
       activeUserCountLoadingStatus: true,
       uniqueUserCountByIDPLoadingStatus: true,
       uniqueUserCountByRealmLoadingStatus: true,
-      totalUserCountLoadingStatus: true
+      totalUserCountLoadingStatus: true,
     };
   },
   async created() {
@@ -135,12 +179,14 @@ export default {
     this.loadTotalNumberOfUsers();
     this.loadUniqueUserCountByIDP();
     this.loadUniqueUserCountByRealm();
-    this.loadActiveTotalUser('7D');
+    this.loadActiveTotalUser("7D");
     this.mergeActiveUsersCountWithRealmsInfo();
   },
   methods: {
     getActiveClass(name) {
-      return this.totalUserCountSelectedFormat == name ? 'primary' : 'secondary';
+      return this.totalUserCountSelectedFormat == name
+        ? "primary"
+        : "secondary";
     },
     async loadRealmsInfo() {
       const response = await RealmsRepository.get();
@@ -162,8 +208,8 @@ export default {
       const labels = [];
       const dataset = [];
       for (var key of response.data.entries()) {
-        labels.push(key[1]["IDP"].padEnd(20))
-        dataset.push(key[1]["UNIQUE_USER_COUNT"])
+        labels.push(key[1]["IDP"].padEnd(20));
+        dataset.push(key[1]["UNIQUE_USER_COUNT"]);
       }
 
       this.uniqueUserCountByIDP["labels"] = labels;
@@ -171,12 +217,14 @@ export default {
       this.uniqueUserCountByIDPLoadingStatus = false;
     },
     async loadUniqueUserCountByRealm() {
-      const response = await MetricsRepository.get("unique-user-count-by-realm");
+      const response = await MetricsRepository.get(
+        "unique-user-count-by-realm"
+      );
       const labels = [];
       const dataset = [];
       for (var key of response.data.entries()) {
-        labels.push(key[1]["REALM"].padEnd(20))
-        dataset.push(key[1]["UNIQUE_USER_COUNT"])
+        labels.push(key[1]["REALM"].padEnd(20));
+        dataset.push(key[1]["UNIQUE_USER_COUNT"]);
       }
 
       this.uniqueUserCountByRealm["labels"] = labels;
@@ -186,11 +234,13 @@ export default {
     async loadActiveTotalUser(format) {
       this.totalUserCountSelectedFormat = format;
       this.totalUserCountLoadingStatus = true;
-      const response = await MetricsRepository.get("total-active-user-count/" + format);
+      const response = await MetricsRepository.get(
+        "total-active-user-count/" + format
+      );
       const labels = [];
       const dataset = [];
       for (var key of response.data.entries()) {
-        labels.push(new Date(key[1]["DATEE"]).toISOString().split('T')[0]);
+        labels.push(new Date(key[1]["DATEE"]).toISOString().split("T")[0]);
         dataset.push(key[1]["ACTIVE_USER_COUNT"]);
       }
       this.totalUserCount["DATEE"] = labels;
@@ -198,20 +248,25 @@ export default {
       this.totalUserCountLoadingStatus = false;
     },
     mergeActiveUsersCountWithRealmsInfo() {
-      this.activeUserCount = this.activeUserCount.map(item => {
-        return { ...item, REALM_DESCRIPTION: this.realmsInfo.find(realm => realm.REALM === item.REALM)?.DESCRIPTION }
-      })
+      this.activeUserCount = this.activeUserCount.map((item) => {
+        return {
+          ...item,
+          REALM_DESCRIPTION: this.realmsInfo.find(
+            (realm) => realm.REALM === item.REALM
+          )?.DESCRIPTION,
+        };
+      });
     },
     handleError(message, error) {
       this.$store.commit("alert/setAlert", {
         message: message + ": " + error,
-        type: "error"
+        type: "error",
       });
       window.scrollTo(0, 0);
     },
     checkEmpty() {
       return true;
-    }
+    },
   },
 };
 </script>
@@ -263,7 +318,7 @@ export default {
   margin: 10px 0;
 }
 
-.theme--light.v-data-table>.v-data-table__wrapper>table>thead>tr>th,
+.theme--light.v-data-table > .v-data-table__wrapper > table > thead > tr > th,
 tbody {
   color: #003366;
 }
