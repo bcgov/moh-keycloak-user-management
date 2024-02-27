1cca950d66576dcc7a7a7f7cc4cf439c640f5fad
filipflorek <filip.florek@cgi.com>
2024-02-27 12:59:29 -0700

    Add RoleEntity, RoleRepository. write Tests

---
+++ backend/src/main/java/ca/bc/gov/hlth/mohums/user/RoleEntity.java
@@ -0,0 +1,106 @@
+package ca.bc.gov.hlth.mohums.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "KEYCLOAK_ROLE", schema = "KEYCLOAK", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"NAME", "CLIENT_REALM_CONSTRAINT"})})
+public class RoleEntity {
+    @Id
+    @Column(name = "ID", length = 36)
+    private String id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    // hax! couldn't get constraint to work properly
+    @Column(name = "REALM_ID")
+    private String realmId;
+
+    @Column(name = "CLIENT_ROLE")
+    private boolean clientRole;
+
+    @Column(name = "CLIENT")
+    private String clientId;
+
+    // Hack to ensure that either name+client or name+realm are unique. Needed due to MS-SQL as it don't allow multiple NULL values in the column, which is part of constraint
+    @Column(name = "CLIENT_REALM_CONSTRAINT", length = 36)
+    private String clientRealmConstraint;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRealmId() {
+        return realmId;
+    }
+
+    public void setRealmId(String realmId) {
+        this.realmId = realmId;
+    }
+
+    public boolean isClientRole() {
+        return clientRole;
+    }
+
+    public void setClientRole(boolean clientRole) {
+        this.clientRole = clientRole;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientRealmConstraint() {
+        return clientRealmConstraint;
+    }
+
+    public void setClientRealmConstraint(String clientRealmConstraint) {
+        this.clientRealmConstraint = clientRealmConstraint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof RoleEntity)) return false;
+
+        RoleEntity that = (RoleEntity) o;
+
+        if (!id.equals(that.getId())) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ backend/src/test/java/ca/bc/gov/hlth/mohums/user/RoleRepositoryTest.java
@@ -0,0 +1,74 @@
+package ca.bc.gov.hlth.mohums.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class RoleRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    private final String umsIntegrationTestsClientId = "24447cb4-f3b1-455b-89d9-26c081025fb9";
+
+    @Test
+    public void testFindByClientIdNotFound() {
+        List<RoleEntity> notFound = roleRepository.findMohApplicationsRolesByClient("non-existing-client-id");
+        assertTrue(notFound.isEmpty());
+    }
+
+    @Test
+    public void testFindByClientIdFromDifferentRealmNotFound() {
+        String masterRealmClient = "0bdb6f27-403c-4510-82b6-c10355426737";
+        List<RoleEntity> notFound = roleRepository.findMohApplicationsRolesByClient(masterRealmClient);
+        assertTrue(notFound.isEmpty());
+    }
+
+    @Test
+    public void testFindByClientIdSuccessful() {
+        List<RoleEntity> roles = roleRepository.findMohApplicationsRolesByClient(umsIntegrationTestsClientId);
+        assertTrue(roles.size() > 0);
+        assertTrue(roles.stream().allMatch(r -> r.getClientId().equals(umsIntegrationTestsClientId) && r.getRealmId().equals("moh_applications")));
+    }
+
+    @Test
+    public void testFindByClientIdAndNamesNotFound() {
+        List<String> nonExistingRoles = List.of("role-a", "role-b");
+        List<RoleEntity> notFound = roleRepository.findMohApplicationsRolesByClientAndNames(umsIntegrationTestsClientId, nonExistingRoles);
+        assertTrue(notFound.isEmpty());
+    }
+
+    @Test
+    public void testFindByClientIdAndEmptyNamesNotFound() {
+        List<String> emptyRoles = List.of();
+        List<RoleEntity> notFound = roleRepository.findMohApplicationsRolesByClientAndNames(umsIntegrationTestsClientId, emptyRoles);
+        assertTrue(notFound.isEmpty());
+    }
+
+    @Test
+    public void testFindByClientIdAndOneNameSuccessful() {
+        List<String> roleName = List.of("TEST_ROLE");
+        List<RoleEntity> rolesResponse = roleRepository.findMohApplicationsRolesByClientAndNames(umsIntegrationTestsClientId, roleName);
+        assertEquals(1, rolesResponse.size());
+        assertTrue(rolesResponse.stream().allMatch(r -> r.getClientId().equals(umsIntegrationTestsClientId) && r.getRealmId().equals("moh_applications") && r.getName().equals(roleName.get(0))));
+    }
+
+    @Test
+    public void testFindByClientIdAndMultipleNameSuccessful() {
+        List<String> roleNames = List.of("TEST_ROLE", "getUsersInRole_TEST_ROLE");
+        List<RoleEntity> rolesResponse = roleRepository.findMohApplicationsRolesByClientAndNames(umsIntegrationTestsClientId, roleNames);
+        assertEquals(roleNames.size(), rolesResponse.size());
+        assertTrue(rolesResponse.stream().allMatch(r -> r.getClientId().equals(umsIntegrationTestsClientId) && r.getRealmId().equals("moh_applications") && roleNames.contains(r.getName())));
+    }
+}
