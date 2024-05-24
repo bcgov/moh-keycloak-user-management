1f97053b959032ec25eaaa6a19fd68f2229c033d
filipflorek <filip.florek@cgi.com>
2024-05-24 15:12:25 -0600

    add alias mapping for PHO-RSC clients

--- frontend/src/api/ClientsRepository.js
+++ frontend/src/api/ClientsRepository.js
@@ -22,4 +22,20 @@ export default {
       )
     );
   },
+
+  assignClientAliases(clients) {
+    let clientAliases = [
+      { clientId: "PHO-RSC", alias: "POSIT-USER-ROLES" },
+      { clientId: "PHO-RSC-GROUPS", alias: "POSIT-GROUP-ROLES" },
+    ];
+    return clients.map((client) => {
+      const aliasMapping = clientAliases.find(
+        (clientAlias) => clientAlias.clientId === client.clientId
+      );
+      if (aliasMapping) {
+        return { ...client, clientId: aliasMapping.alias };
+      }
+      return client;
+    });
+  },
 };
