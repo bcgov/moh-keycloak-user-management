c05edb5f66b168c4085e28701984e4d9cf2059a2
filipflorek <filip.florek@cgi.com>
2024-11-14 10:01:04 -0700

    import vuetify components and directives

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import "vuetify/styles";
 
 export default createVuetify({
+  components,
+  directives,
   theme: {
     themes: {
       light: {
