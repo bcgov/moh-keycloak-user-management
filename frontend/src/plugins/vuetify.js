a1813f109710ae2b68a181aec02c9dd63198fd7f
filipflorek <filip.florek@cgi.com>
2025-01-13 20:21:19 -0700

    scss variables updates

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,23 +1,28 @@
+import "@/scss/variables.scss";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "vuetify/styles";
 
+const lightTheme = {
+  dark: false,
+  colors: {
+    primary: "#003366",
+    secondary: "#38598a",
+    textLink: "#1A5A96",
+    red: "#D8292F",
+    success: "#2E8540",
+    background: "#F1F1F1",
+  },
+};
+
 export default createVuetify({
   components,
   directives,
   theme: {
+    defaultTheme: "lightTheme",
     themes: {
-      light: {
-        colors: {
-          primary: "#003366",
-          secondary: "#38598a",
-          textLink: "#1A5A96",
-          red: "#D8292F",
-          success: "#2E8540",
-          background: "#F1F1F1",
-        },
-      },
+      lightTheme,
     },
   },
 });
